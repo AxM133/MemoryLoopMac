e0592ac472a381b0c30768e498704313472a3e3e
Azim <axm7311@gmail.com>
2025-11-12 13:00:46 +0300

    Initial Commit

---
+++ MemoryLoopMac/MemoryLoopMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryLoopMacApp.swift
+//  MemoryLoopMac
+//
+//  Created by axm on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLoopMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
